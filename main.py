bb60a08015355db68344c2b54324a599644b003c
LarsPerkins <lars@larsperkins.com>
2021-01-06 09:29:43 -0500

    fix landing calculations

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from math import radians, cos, sin, asin, sqrt
 
 trans_dict = {
     'BOWLESAGAWAM': '7B0',
+    "BOWLES": "",
+    "AGAWAM": "7B0",
     'OXFORD': '43M',
     'WICHABAI': 'SYWI',
     'WICHIBAI': 'SYWI',
     'NORTHADAMS': 'AQW',
-    'NORTHCENTRAL': 'SFZ',
+    "N.": "",
+    "ADAMS": "AQW",
+    'NORTHCENTRAL': 'KSFZ',
+    "NORTH": "",
+    "CENTRAL": "KSFZ",
     'SHIRLEY': '61MA',
     'MARSTONMILLS': '2B1',
     'BARRE': '8B5',
@@ -29,9 +35,22 @@ trans_dict = {
     "40N": "KMQS",
     "PUT": "",
     "VOR": "",
+    "N": "",
     "PLACEHOLDER": "",
     "LOGBOOK": "",
     "PIRU": "",
+    "FLEUR": "KWVL",
+    "CAPE": "KCQX",
+    "PLYMOUTH": "KPYM",
+    "PTOWN": "KPVC",
+    "HAMPTON": "7B2",
+    "LA": "",
+    "MIDDLETOWN": "KUUU",
+    "MILLS": "",
+    "MARSTON": "2B1",
+    "CO": "",
+    "ESSEX": "KCDW",
+    "MA": "",
 }
 
 
@@ -122,13 +141,11 @@ def load_airport_data():
 
 
 def translate_identifier(identifier):
-    identifier = trans_dict.get(identifier, identifier)
+    identifier = trans_dict.get(identifier.upper(), identifier.upper())
     if len(identifier) == 3 and not(any(c.isnumeric() for c in identifier)):
         identifier = "K" + identifier
-    identifier = "UNKN" if not identifier else identifier
 
-    return identifier.upper()
-
+    return identifier
 
 input_flights = []
 aircraft_table = []
@@ -158,6 +175,45 @@ for flight in input_flights:
         origin_airport = translate_identifier(flight[2])
         destination_airport = translate_identifier(flight[3])
 
+        day_takeoffs = int(flight[18]) if flight[18] else 0
+        day_landings = int(flight[19]) if flight[19] else 0
+        night_takeoffs = int(flight[20]) if flight[20] else 0
+        night_landings = int(flight[21]) if flight[21] else 0
+
+        night_time = float(flight[14]) if flight[14] else 0.0
+        total_time = float(flight[11]) if flight[11] else 0.0
+
+        night_flight = False
+        partial_night_flight = False
+        day_flight = False
+
+        if night_time == total_time and total_time != 0.0:
+            night_flight = True
+            day_takeoffs = 0
+            day_landings = 0
+            night_landings = max(1, night_landings)
+            night_takeoffs = night_landings
+
+        elif night_time > 0.0:
+            partial_night_flight = True
+            day_takeoffs = max(1, day_takeoffs)
+            day_landings = 0
+            night_landings = max(1, night_landings)
+            night_takeoffs = (day_takeoffs + night_takeoffs) - day_takeoffs
+
+        else:
+            day_flight = True
+            night_takeoffs = 0
+            night_landings = 0
+            day_landings = max(1, day_landings)
+            day_takeoffs = day_landings
+
+        flight[18] = day_takeoffs
+        flight[19] = day_landings
+        flight[20] = night_takeoffs
+        flight[21] = night_landings
+        flight[22] = day_landings + night_landings
+
         total_input_flight_distance = distance_between_airports(origin_airport, destination_airport, airport_lat_long_dict)
 
         flight[2] = origin_airport
@@ -174,9 +230,12 @@ for flight in input_flights:
         if valid and flight_datetime < datetime(2016, 1, 1) and flight[0] != "Date":
             flight_route = flight[4]
             route_segments = flight_route.split(' ')
+            route_segments = [trans_dict.get(x.upper(), x.upper()) for x in route_segments]
+            route_segments = [x for x in route_segments if x]
             route_has_segments_to_split = all(len(x) == 3 or len(x) == 4 for x in route_segments)
 
             if route_has_segments_to_split:
+
                 starting_airport = origin_airport
                 total_number_of_segments = len(route_segments) + 1
 
@@ -199,10 +258,33 @@ for flight in input_flights:
                     segment_flight[2] = segment_origin_airport
                     segment_flight[3] = segment_destination_airport
 
-                    for j in range(18, 23):
-                        landing_count = int(flight[j]) if flight[j] else 0
-                        segment_flight[j] = 1 if landing_count > 1 else landing_count
+                    # day takeoffs 18
+                    # day landings 19
+                    # night takeoffs 20
+                    # night landings 21
 
+                    # if night time (14) == total time (11) night t/o = max(1, night t/o), night ldg = max(1, night ldg)
+                    # if night > 0 day t/o = max(1, night t/o), night ldg = max(1, night ldg)
+
+                    if day_flight:
+                        segment_flight[18] = 1
+                        segment_flight[19] = 1
+                        segment_flight[20] = 0
+                        segment_flight[21] = 0
+
+                    elif partial_night_flight:
+                        segment_flight[18] = 1
+                        segment_flight[19] = 0
+                        segment_flight[20] = 0
+                        segment_flight[21] = 1
+
+                    else:
+                        segment_flight[18] = 0
+                        segment_flight[19] = 0
+                        segment_flight[20] = 1
+                        segment_flight[21] = 1
+
+                    segment_flight[22] = segment_flight[19] + segment_flight[21]
                     segment_flight[17] = segment_distance
                     segment_flights.append(segment_flight)
                     total_segments_flight_distance += segment_distance
@@ -227,15 +309,23 @@ for flight in input_flights:
     else:
         output_flights.append(flight)
 
+"""
 for flight in output_flights:
     for i in range(18, 22):
         try:
-            flight[i] = 0 if not flight[i] else int(flight[i])
+            flight[i] = int(flight[i]) if flight[i] else 0
 
         except ValueError:
+            flight[i] = 0
             pass
 
     try:
+
+        # day takeoffs 18
+        # day landings 19
+        # night takeoffs 20
+        # night landings 21
+
         total_takeoffs = (int(flight[18]) + int(flight[20]))
         total_landings = (int(flight[19]) + int(flight[21]))
 
@@ -245,11 +335,12 @@ for flight in output_flights:
         if total_landings > total_takeoffs:
             flight[18] = int(flight[18]) + total_landings - total_takeoffs
 
-        flight[18] = int() if (int(flight[18]) + int(flight[20])) == 0 else flight[18]
+        flight[18] = int(flight[19]) + int(flight[21]) if (int(flight[18]) + int(flight[20])) == 0 else flight[18]
         flight[19] = int(flight[18]) + int(flight[20]) if (int(flight[19]) + int(flight[21])) == 0 else flight[19]
 
     except ValueError:
         pass
+"""
 
 with open(f'logbook-output.csv', 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile, delimiter=',',
